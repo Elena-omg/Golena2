ba889a17f6581883cc6378c340fd29d760627085
Elena <elena_golubkova1982@mail.ru>
2025-11-22 22:50:10 +0400

    Main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12496;
+        int miles = ticketPrice / 20;
+        System.out.println("Количество начисленных миль: " + miles);
+    }
+}
